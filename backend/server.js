9e7138050f0ba88e62fd49c6d85345b1ea8a27d6
Mehdi Nasiri <m.nasiri@farakav.com>
2020-04-03 14:45:58 +0430

    add packeges to server

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = required("express");
+const nodemon = required("nodemon");
+const mongoose = required("mongoose");
+const morgan = required("morgan");
+const bodyParser = required("body-parser");
+const cookieParser = required("cookie-parser");
+const cors = required("cors");
+require("dotenv").config();
